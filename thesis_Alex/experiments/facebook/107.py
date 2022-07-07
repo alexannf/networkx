0f7a8c8c1aaea8cd73fea64f015d2f16e22ef14b
alexannf <alexannf@stud.ntnu.no>
2022-07-07 19:40:03 +0200

    reduced size of edge stream to be able to run within reasonable time

--- thesis_Alex/experiments/facebook/107.py
+++ thesis_Alex/experiments/facebook/107.py
@@ -1,5 +1,5 @@
 from networkx.algorithms.thesis_Alex.thesis import setup, puzis_state_of_the_art_add
 
 if __name__ == '__main__':
-    G, edge_stream, groups = setup(6749, 20, 20, "facebook", "107")
-    puzis_state_of_the_art_add(G, edge_stream, groups, 6749, 20, 20, "facebook", "107")
+    G, edge_stream, groups = setup(3000, 20, 20, "facebook", "107")
+    puzis_state_of_the_art_add(G, edge_stream, groups, 3000, 20, 20, "facebook", "107")
