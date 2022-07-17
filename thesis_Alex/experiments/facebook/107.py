55157a5efb7a163b949355b9c291cbad2c08b3d1
alexannf <alexannf@stud.ntnu.no>
2022-07-17 20:05:14 +0200

    107 ready

--- thesis_Alex/experiments/facebook/107.py
+++ thesis_Alex/experiments/facebook/107.py
@@ -5,9 +5,9 @@ from networkx.algorithms.thesis_Alex.data import get_data
 
 if __name__ == '__main__':
     edge_stream_add, groups_add = get_data()  # 10 x 10 groups, edge stream size 2000, facebook 107
-    G_ad = setup_2("add", edge_stream_add, "facebook", "107")
+    G_add = setup_2("add", edge_stream_add, "facebook", "107")
 
-    # puzis_state_of_the_art_add(G_add, edge_stream_add, groups_add, "facebook", "107")
+    puzis_state_of_the_art_add(G_add, edge_stream_add, groups_add, "facebook", "107")
     # thesis_add_gbc(G_add, edge_stream_add, groups_add, "facebook", "107", space=False)
     #
     # puzis_state_of_the_art_remove(G_rem, edge_stream_rem, groups_rem, "facebook", "107")
