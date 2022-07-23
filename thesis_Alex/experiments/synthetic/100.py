da50685b841fbb0860d6ef5b010770af6a90a6b6
alexannf <alexannf@stud.ntnu.no>
2022-07-23 11:22:08 +0200

    rewrote experiment setup to not so easily allow for typos, new space config must still be written

--- thesis_Alex/experiments/synthetic/100.py
+++ thesis_Alex/experiments/synthetic/100.py
@@ -4,11 +4,11 @@ from networkx.algorithms.thesis_Alex.setup import *
 
 if __name__ == '__main__':
     category = "synthetic"
-    dataset = "1000"
-    edg_strm_siz = 100
-    grp_siz = 10
-    nm_grps = 10
-    
+    dataset = "100"
+    edg_strm_siz = 20
+    grp_siz = 5
+    nm_grps = 5
+
     G_add, edge_stream_add, groups_add = setup("add", edg_strm_siz, grp_siz, nm_grps, category, dataset)
     G_rem = setup_2("remove", edge_stream_add, category, dataset)
     edge_stream_rem, groups_rem = list(reversed(edge_stream_add)), groups_add
@@ -24,5 +24,3 @@ if __name__ == '__main__':
 
     puzis_state_of_the_art_remove(G_rem, edge_stream_rem, groups_rem, category, dataset, space=True)
     thesis_remove_gbc(G_rem, edge_stream_rem, groups_rem, category, dataset, space=True)
-
-
